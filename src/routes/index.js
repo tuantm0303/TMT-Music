e5824bf2aa2c7c2161bb2e8e99474905382de676
tuantm0303 <minhtuan0330@gmail.com>
2023-01-05 21:04:25 +0700

    feat: route mymusic

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+import config from "../config";
+import Home from "../pages/Home";
+import MyMusic from "../pages/MyMusic";
+
+export const publicRouter = [
+  { path: config.routes.home, component: Home },
+  { path: config.routes.mymusic, component: MyMusic },
+];
