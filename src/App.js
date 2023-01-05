c1ba015792ff5d5d44cdd685af6b3154a5c41fbd
tuantm0303 <minhtuan0330@gmail.com>
2023-01-05 21:30:15 +0700

    refactor: routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import MyMusic from "./pages/MyMusic";
+import { publicRoutes } from "./routes";
 
 function App() {
   return (
     <div className="content flex w-full bg-[#170F23]">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/mymusic" element={<MyMusic />} />
+        {publicRoutes.map((route, index) => {
+          const Component = route.component;
+          return (
+            <Route
+              key={index}
+              path={route.path}
+              element={<Component />}
+            ></Route>
+          );
+        })}
       </Routes>
     </div>
   );
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import config from "../config";
 import Home from "../pages/Home";
 import MyMusic from "../pages/MyMusic";
 
-export const publicRouter = [
+export const publicRoutes = [
   { path: config.routes.home, component: Home },
   { path: config.routes.mymusic, component: MyMusic },
 ];
