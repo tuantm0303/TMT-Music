bfd1b5c6856ea97d8ea51a5c8df4f9bef2b6febf
tuantm0303 <minhtuan0330@gmail.com>
2023-01-09 17:08:12 +0700

    style: refactor UI

--- src/components/Player/components/Song/index.js
+++ src/components/Player/components/Song/index.js
@@ -2,20 +2,21 @@ import React from "react";
 import { AiOutlineHeart } from "react-icons/ai";
 import { RiMoreFill } from "react-icons/ri";
 
-const Song = (prop) => {
+const Song = ({ title, singer, time }) => {
   return (
     <>
-      <div className="song flex items-center h-[56px] w-full p-2 hover:bg-slate-500 hover:rounded-lg">
+      <div className="song group flex items-center h-[60px] w-full p-2 hover:bg-slate-500 hover:rounded-lg">
         <img
           src="https://photo-resize-zmp3.zmdcdn.me/w94_r1x1_webp/cover/8/d/8/9/8d8925bf78937c966bd9a25678a4dbe4.jpg"
           alt=""
-          className="w-10 h-10 rounded-lg hover:opacity-60"
+          className="w-10 rounded-lg group-hover:opacity-60"
         />
-        <div className="text pl-5 flex-grow-[1]">
-          <p className="title-song font-bold">{prop.title}</p>
-          <span className="singer opacity-60 hover:underline hover:text-purple-500">
-            {prop.singer}
-          </span>
+        <div className="text pl-5 flex-grow-[1] h-10 leading-[10px]">
+          <p className="title-song font-bold text-sm">{title}</p>
+          <p className="singer opacity-60 hover:underline hover:text-purple-500 text-xs py-0.5">
+            {singer}
+          </p>
+          <span className="time text-xs">{time}</span>
         </div>
         <div className="icons-hidden hidden hover:inline-flex">
           <AiOutlineHeart />
--- src/pages/MyMusic/index.js
+++ src/pages/MyMusic/index.js
@@ -1,7 +1,54 @@
 import React from "react";
+import { BsPlayCircle } from "react-icons/bs";
+import { CiMusicNote1 } from "react-icons/ci";
+import Song from "../../components/Player/components/Song";
 
 const MyMusic = () => {
-  return <div>MyMusic</div>;
+  return (
+    <>
+      <main className="main-board pt-16 px-10">
+        <div className="title flex">
+          <span className="text-6xl font-bold pr-4">Thư viện</span>
+          <BsPlayCircle className="text-6xl font-bold rounded-full bg-violet-700 hover:opacity-50" />
+        </div>
+
+        <ul className="menu flex pt-16 justify-items-start border-b-2 border-b-gray-500 pb-3">
+          <li className="pr-8 hover:opacity-90 active:border-2">BÀI HÁT</li>
+          <li className="pr-8 hover:opacity-90 active:border-2">PODCAST</li>
+          <li className="pr-8 hover:opacity-90 active:border-2">ALBUM</li>
+          <li className="pr-8 hover:opacity-90 active:border-2">MV</li>
+        </ul>
+        <ul className="genre-select flex py-5 justify-start">
+          <li className="selector text-xs">
+            <button className="rounded-full border-2 p-1 px-4 active:bg-violet-700 hover:opacity-30">
+              YÊU THÍCH
+            </button>
+          </li>
+          <li className="selector mx-8 text-xs">
+            <button className="rounded-full border-2 p-1 px-4 active:bg-violet-700 hover:opacity-30">
+              ĐÃ TẢI LÊN
+            </button>
+          </li>
+        </ul>
+
+        <div className="media header flex justify-around pb-4 border-b-2 border-b-gray-500 pt-5">
+          <h3 className="media-right opacity-50">BÀI HÁT</h3>
+          <h3 className="media-mid opacity-50">ALBUM</h3>
+          <h3 className="media-left opacity-50">THỜI GIAN</h3>
+        </div>
+        <div className="media-container">
+          <div className="media-song border-b-2 border-b-gray-500 flex items-center hover:bg-slate-500 hover:rounded-lg">
+            <CiMusicNote1 className="w-8" />
+            <Song title="Già Cùng Nhau Là Được" singer="Tây Nguyên Sound" />
+          </div>
+          <div className="media-song border-b-2 border-b-gray-500 flex items-center hover:bg-slate-500 hover:rounded-lg">
+            <CiMusicNote1 className="w-8" />
+            <Song title="Già Cùng Nhau Là Được" singer="Tây Nguyên Sound" />
+          </div>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default MyMusic;
