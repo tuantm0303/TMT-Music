1371be93bbe16a17a3f32b85d4cf463472a7ade4
tuantm0303 <minhtuan0330@gmail.com>
2023-01-14 23:43:28 +0700

    feat: config basic redux & instance services

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./rootReducer";
+// import reducer from "./rootReducer";
+
+const store = configureStore({
+  // DÙNG ĐỂ CẬP NHẬT LẠI GIÁ TRỊ STATE THÔNG QUA ACTION
+  // là function nhận vào hai đối số là state và action.
+  // sử dụng để cập nhật state trong store
+  // kiểm tra action để cập nhật lại state tương ứng
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export default store;
