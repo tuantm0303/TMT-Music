f6247ad41257e9362fbaa436d2f91739b6af9317
tuantm0303 <minhtuan0330@gmail.com>
2023-02-23 21:47:16 +0700

    feat: config redux-persist

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import persistStore from "redux-persist/es/persistStore";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./store";
 
+let persistor = persistStore(store);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>
 );
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,12 +1,22 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import categoryReducer from "./features/categorySlice";
 import songReducer from "./features/songSlice";
 import singerReducer from "./features/singerSlice";
 
-const rootReducer = combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  // whitelist: ["songReducer"],
+};
+
+const reducers = combineReducers({
   categoryReducer,
   songReducer,
   singerReducer,
 });
+
+const rootReducer = persistReducer(persistConfig, reducers);
 
 export default rootReducer;
