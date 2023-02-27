27b32cd9210e18a8325d2bb424e6540deefef38f
tuantm0303 <minhtuan0330@gmail.com>
2023-02-27 23:30:23 +0700

    style: fix UI slider

--- src/components/Banner/Slide.js
+++ src/components/Banner/Slide.js
@@ -3,8 +3,12 @@ import { HStack, Text } from "@chakra-ui/react";
 const Slide = ({ img, name }) => {
   return (
     <HStack alignContent="center" justifyContent="center" mx="4">
-      <Text style={{ width: "319px" }}>
-        <img src={img} alt={name} style={{ borderRadius: 16 }} />
+      <Text>
+        <img
+          src={img}
+          alt={name}
+          style={{ width: 320, height: 180, borderRadius: 16 }}
+        />
       </Text>
     </HStack>
   );
