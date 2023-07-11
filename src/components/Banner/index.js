a95b511bc34aa5c730a5bcb5d7f550454808d7c4
tuantm0303 <minhtuan0330@gmail.com>
2023-07-11 15:21:58 +0700

    refactor: banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,14 +1,9 @@
-import { Box } from "@chakra-ui/react";
-import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
+import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import { slidesApi } from "../../services/slide";
-import Slide from "./Slide";
-
-const Slider = dynamic(() => import("react-slick").then((m) => m.default), {
-  ssr: false,
-});
+import './Banner.scss';
 
 const Banner = () => {
   const [slides, setSlides] = useState([]);
@@ -19,68 +14,49 @@ const Banner = () => {
     })();
   }, []);
 
-  const slickSettings = {
+  const settings = {
     dots: true,
     infinite: true,
     speed: 1000,
     autoplay: true,
     slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
+
   return (
     <div className="kxbovgjmpw">
-      <Box m="20">
-        <Box
-          sx={{
-            ".slick-dots": {
-              transform: "translateY(1em)",
-            },
-            ".slick-dots li button": {
-              _before: {
-                transition: "0.2s",
-                content: "''",
-                borderRadius: "100%",
-                background: "cyan.500",
-              },
-            },
-            ".slick-arrow": {
-              backgroundColor: "cyan.500",
-              color: "white",
-              w: "30px",
-              h: "50px",
-              transition: "0.2s",
-              _hover: {
-                backgroundColor: "cyan.500",
-                color: "white",
-              },
-              _focus: {
-                backgroundColor: "cyan.500",
-                color: "white",
-              },
-              _before: {
-                transition: "0.2s",
-              },
-            },
-            ".slick-prev": {
-              left: "-40px",
-              _before: {
-                content: '"◀"',
-              },
-            },
-            ".slick-next": {
-              right: "-40px",
-              _before: {
-                content: '"▶"',
-              },
-            },
-          }}
-        >
-          <Slider {...slickSettings}>
-            {slides.map((item, index) => (
-              <Slide key={index} img={item.image} name={item.name} />
-            ))}
-          </Slider>
-        </Box>
-      </Box>
+      <Slider {...settings}>
+        {slides.map((item, index) => (
+          <div className="ohwxinitqm" key={index}>
+            <img src={item?.image} alt={item?.name} />
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
