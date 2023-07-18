8e76b67ea985392b602586ef0b8b47e562ace62f
tuantm0303 <minhtuan0330@gmail.com>
2023-07-18 14:21:16 +0700

    add

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,6 @@ import { Button } from "antd";
 import { useEffect, useState } from "react";
 import logo from '../../assets/images/logo.png';
 import { menuApi } from "../../services/menu";
-import ItemNavbar from "./ItemNavbar";
 import './Navbar.scss';
 
 const Navbar = () => {
@@ -20,18 +19,18 @@ const Navbar = () => {
       <div className="jkdzfirhal">
         <img src={logo} alt="" />
       </div>
-      <ItemNavbar data={data} location='top' />
+      {/* <ItemNavbar data={data} location='top' /> */}
       <hr className="iakqsebcro" />
       <div className="qyotbfaklt">
-        <ItemNavbar data={data} location='middle' />
+        {/* <ItemNavbar data={data} location='middle' /> */}
         <div className="zjrxcwxdha">
           <div className="qdsfbritlb">Nghe nhạc không quảng cáo cùng kho nhạc PREMIUM</div>
           <Button className="mrhmmaomvh">Nâng cấp tài khoản</Button>
         </div>
-        <ItemNavbar data={data} location='bottom' />
+        {/* <ItemNavbar data={data} location='bottom' /> */}
       </div>
       <div className="gmydssuzrk">
-        <ItemNavbar data={data} location='relative' />
+        {/* <ItemNavbar data={data} location='relative' /> */}
       </div>
     </div>
   )
