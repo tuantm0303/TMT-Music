c28df9c67e5cff6b8727bc3834ef964a7f9437b5
tuantm0303 <minhtuan0330@gmail.com>
2023-07-18 14:50:49 +0700

    remove IIFE

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,34 +1,38 @@
 import { Button } from "antd";
+import { useEffect, useState } from "react";
 import logo from '../../assets/images/logo.png';
+import { menuApi } from "../../services/menu";
+import ItemNavbar from "./ItemNavbar";
 import './Navbar.scss';
 
 const Navbar = () => {
-  // const [data, setData] = useState([])
+  const [data, setData] = useState([])
 
-  // useEffect(() => {
-  //   (async () => {
-  //     const { data } = await menuApi.list()
-  //     setData(data)
-  //   })()
-  // }, [])
+  const getMenu = async () => {
+    const { data } = await menuApi.list()
+    setData(data)
+  }
+  useEffect(() => {
+    getMenu()
+  }, [])
 
   return (
     <div className="mnujqleebm">
       <div className="jkdzfirhal">
         <img src={logo} alt="" />
       </div>
-      {/* <ItemNavbar data={data} location='top' /> */}
+      <ItemNavbar data={data} location='top' />
       <hr className="iakqsebcro" />
       <div className="qyotbfaklt">
-        {/* <ItemNavbar data={data} location='middle' /> */}
+        <ItemNavbar data={data} location='middle' />
         <div className="zjrxcwxdha">
           <div className="qdsfbritlb">Nghe nhạc không quảng cáo cùng kho nhạc PREMIUM</div>
           <Button className="mrhmmaomvh">Nâng cấp tài khoản</Button>
         </div>
-        {/* <ItemNavbar data={data} location='bottom' /> */}
+        <ItemNavbar data={data} location='bottom' />
       </div>
       <div className="gmydssuzrk">
-        {/* <ItemNavbar data={data} location='relative' /> */}
+        <ItemNavbar data={data} location='relative' />
       </div>
     </div>
   )
