51338636d911ce004eae276c2bc95059533de375
tuantm0303 <minhtuan0330@gmail.com>
2024-03-15 23:33:38 +0700

    fix icon

--- src/components/Player/Song.js
+++ src/components/Player/Song.js
@@ -1,25 +1,22 @@
-import { Popover } from 'antd';
-import { useDispatch, useSelector } from 'react-redux';
-import { ReactComponent as IconHeart } from '../../assets/svgs/icon-heart.svg';
-import { ReactComponent as IconOption } from '../../assets/svgs/icon-option.svg';
-import { ReactComponent as IconPlay } from '../../assets/svgs/icon-play.svg';
-import { addFavourites } from '../../store/features/songSlice';
-import './Song.scss';
+import { Popover } from "antd"
+import { useDispatch, useSelector } from "react-redux"
+import { ReactComponent as IconHeart } from "../../assets/svgs/icon-heart.svg"
+import { ReactComponent as IconOption } from "../../assets/svgs/icon-option.svg"
+import { ReactComponent as IconPlay } from "../../assets/svgs/icon-play.svg"
+import { addFavourites } from "../../store/features/songSlice"
+import "./Song.scss"
 
 const Song = ({ image, title, singer }) => {
-  const { favourite } = useSelector((state) => state.songReducer)
-  const dispatch = useDispatch()
-  console.log('favorite', favourite);
-
-  const handleAddFavourites = () => {
-    dispatch(addFavourites(1))
-  }
   return (
     <div className="kybqblbsmr">
       <div className="ibymmdlsiw">
         <div className="fyaiadzrcb">
-          <img src={image} alt="" className="gfmutzlmjv" />
-          <IconPlay className='foxyurbrwb' />
+          <img
+            src={image}
+            alt=""
+            className="gfmutzlmjv"
+          />
+          <IconPlay className="foxyurbrwb" />
         </div>
         <div className="ionozpfoim">
           <div className="psvgsgslfp">{title}</div>
@@ -28,17 +25,26 @@ const Song = ({ image, title, singer }) => {
       </div>
 
       <div className="jlbgbmkehm">
-        <Popover trigger='hover' content={<div style={{ whiteSpace: 'nowrap' }}>Thêm vào thư viện</div>}>
-          <div className='akrlfhazbs' onClick={() => handleAddFavourites()}>
-            <IconHeart />
+        <Popover
+          trigger="hover"
+          content={<div style={{ whiteSpace: "nowrap" }}>Thêm vào thư viện</div>}
+        >
+          <div
+            className="akrlfhazbs"
+            onClick={() => handleAddFavourites()}
+          >
+            <IconHeart className="akrlfhazbs" />
           </div>
         </Popover>
-        <Popover trigger='hover' content='Khác'>
-          <IconOption className='akrlfhazbs' />
+        <Popover
+          trigger="hover"
+          content="Khác"
+        >
+          <IconOption className="akrlfhazbs" />
         </Popover>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Song;
+export default Song
